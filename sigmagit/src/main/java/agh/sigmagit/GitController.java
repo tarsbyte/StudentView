43004cabac3309ed4d476819afaf411dff11ef19
Archflayd <krzysztofxkwiecien@gmail.com>
2022-04-05 20:53:44 +0200

    Login REST

---
+++ sigmagit/src/main/java/agh/sigmagit/GitController.java
@@ -0,0 +1,9 @@
+package agh.sigmagit;
+
+public class GitController {
+
+    public static boolean generateRepository(String repositoryName){
+        // create repository
+        return true; // if repository was created successfully
+    }
+}

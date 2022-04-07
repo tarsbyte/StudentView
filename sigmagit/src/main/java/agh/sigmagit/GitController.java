35f2e854df8493475d18fb65770827d152942492
darnok00 <majunia28@gmail.com>
2022-04-07 19:17:55 +0200

    frontend add

--- sigmagit/src/main/java/agh/sigmagit/GitController.java
+++
@@ -1,9 +0,0 @@
-package agh.sigmagit;
-
-public class GitController {
-
-    public static boolean generateRepository(String repositoryName){
-        // create repository
-        return true; // if repository was created successfully
-    }
-}
